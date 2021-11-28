d4b1627e03b515802b14144ae2327f377ffa42f9
Rkda8071 <42936512+Rkda8071@users.noreply.github.com>
2021-11-28 18:01:22 +0900

    Add files via upload

---
+++ board.py
@@ -0,0 +1,84 @@
+class Board:
+
+    def __init__(self):
+        self.direction = 0
+        self.status = [[0]*17 for i in range(17)]
+        self.status[0][8] = 2
+        self.status[16][8] = 3
+    
+    # 벽 설치!
+    def wall_clicked(self, y, x):
+        # 범위 확인
+        if (not (y%2 and x%2)) or x<=0 or y<=0 or x>=16 or y>=16:
+            return False
+        # 놓을 곳이 비어있는지(벽이 이미 설치되어있는지) 확인
+        for tmp in [0, -1, 1]:
+            if self.status[y + tmp*self.direction][x + tmp*(1 - self.direction)] == 1:
+                return False
+        for tmp in [0, -1, 1]:
+            self.status[y + tmp*self.direction][x + tmp*(1 - self.direction)] = 1
+        # 모든 경로가 막혀있나? bfs로 확인!
+        (i1,j1) = self.find_board(2)
+        (i2,j2) = self.find_board(3)
+        if self.bfs(i1,j1,16) and self.bfs(i2,j2,0):
+            return True
+        for tmp in [0, -1, 1]:
+            self.status[y + tmp*self.direction][x + tmp*(1 - self.direction)] = 0
+        # 이동할 수 있는 경로가 막힙니다!
+        return False
+
+    def bfs(self, y, x, e):
+        q = [(y, x)]
+        tmp = [0,0,1,-1]
+        chk = [[0]*17 for i in range(17)]
+        while len(q) > 0:
+            (y,x) = q.pop()
+            if y == e: return True
+            for i in range(4):
+                yy = y + tmp[i]
+                xx = x + tmp[3-i]
+                if not (yy >= 0 and yy < 17 and xx >= 0 and xx < 17 and self.status[yy][xx] == 0 and chk[yy+tmp[i]][xx+tmp[3-i]] == 0): continue
+                print(yy,xx)
+                chk[yy+tmp[i]][xx+tmp[3-i]] = 1
+                q.append((yy+tmp[i],xx+tmp[3-i]))
+            pass
+        return False
+
+    def player_clicked(self, y, x):
+        if self.status[y][x] == 4:
+            return True
+        return False
+
+    def whereCanMove(self, turn):
+        (y, x) = self.find_board(turn)
+        tmp = [0,0,1,-1]
+        res = []
+        for i in range(4):
+            yy = y + tmp[3-i]; xx = x + tmp[i]
+            if not (yy >= 0 and yy < 17 and xx >= 0 and xx < 17 and self.status[yy][xx] == 0): continue
+            yy += tmp[3-i]; xx += tmp[i]
+
+            if 2 <= self.status[yy][xx] <= 3:
+                #상대편의 말이 있다!
+                for i in range(4):
+                    yyy = yy + tmp[3-i]; xxx = xx + tmp[i]
+                    if not (yyy >= 0 and yyy < 17 and xxx >= 0 and xxx < 17 and self.status[yyy][xxx] == 0): continue
+                    yyy += tmp[3-i]; xxx += tmp[i]
+                    if yyy == y and xxx == x: continue
+                    self.status[yyy][xxx] = 4
+                    res.append((yyy,xxx))
+            else:
+                self.status[yy][xx] = 4
+                res.append((yy, xx))
+        return res
+
+    def find_board(self, tar):
+        for i in range(17):
+            for j in range(17):
+                if self.status[i][j] == tar:
+                    return (i,j)
+
+    def set_direction(self, dir): # 가로 : 0, 세로 : 1
+        self.direction = dir
+        pass
+
---
+++ game.py
@@ -0,0 +1,166 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QGestureRecognizer, QWidget
+from PyQt5.QtWidgets import QLayout, QGridLayout
+from PyQt5.QtWidgets import QTextEdit, QLineEdit, QToolButton
+from PyQt5.QtWidgets import (QWidget, QPushButton,
+    QHBoxLayout, QVBoxLayout, QApplication, QLabel,
+    QComboBox, QTextEdit, QLineEdit)
+
+from board import Board
+
+class QuoridorGame(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        # init Layout
+        quoridorLayout = QGridLayout()
+        self.setLayout(quoridorLayout)
+        
+        # init board_button
+        self.board_button = []
+        for y in range(17):
+            tmp_board = []
+            for x in range(17):
+                tmp_button = QPushButton()
+                tmp_button.clicked.connect(self.btnclick)
+                if y%2 or x%2:
+                    tmp_button.setStyleSheet("background-color: gray")
+                else:
+                    tmp_button.setStyleSheet("background-color: white")
+                
+                quoridorLayout.addWidget(tmp_button, y, x)
+                tmp_board.append(tmp_button)
+            self.board_button.append(tmp_board)
+        
+        # Display widget for current status
+        self.player1block = QLineEdit()
+        self.player1block.setReadOnly(True)
+        quoridorLayout.addWidget(self.player1block, 0, 18)
+        
+        self.statusText = QLineEdit()
+        self.statusText.setReadOnly(True)
+        quoridorLayout.addWidget(self.statusText, 6, 18)
+        
+        self.change_btn = QToolButton()
+        self.change_btn.setText("가로")
+        self.change_btn.clicked.connect(self.change_dir)
+        quoridorLayout.addWidget(self.change_btn, 8, 18)
+        
+        self.currentturn = QLineEdit()
+        quoridorLayout.addWidget(self.currentturn, 10, 18)
+
+
+        self.player2block = QLineEdit()
+        self.player2block.setReadOnly(True)
+        quoridorLayout.addWidget(self.player2block, 16, 18)
+        self.startGame()
+
+    def startGame(self):
+        self.board = Board()
+        self.turn = 2 # player1
+        self.board.whereCanMove(self.turn)
+        self.color_4()
+
+        self.wall_count = [10,10]
+        self.player1block.setText(str(self.wall_count[0]))
+        self.player2block.setText(str(self.wall_count[1]))
+        
+        self.currentturn.setText("Red turn")
+        self.currentturn.setStyleSheet("color : Red")
+
+        self.board_button[0][8].setStyleSheet("background-color : red")
+        self.board_button[16][8].setStyleSheet("background-color : blue")
+    
+    def change_dir(self):
+        btn = self.sender()
+        btn.setText("가로" if btn.text() == "세로" else "세로")
+        self.board.set_direction(0 if btn.text() == "가로" else 1)
+
+    def btnclick(self):
+        (y, x) = self.find_btn(self.sender())
+        print(y, x)
+
+        if y % 2 or x % 2:  # 벽 세우는 곳임
+            if self.wall_count[self.turn - 2] <= 0:
+                self.statusText.setText("벽이 부족합니다!")
+                # 벽이 없습니다
+                return
+            if self.board.wall_clicked(y, x):
+                # 설치 & status 변경
+                for tmp in [0, -1, 1]:
+                    yy = y + tmp*self.board.direction
+                    xx = x + tmp*(1 - self.board.direction)
+                    self.board.status[yy][xx] = 1
+                    self.board_button[yy][xx].setStyleSheet("background-color : brown")
+                # 벽 개수 감소
+                self.wall_count[self.turn - 2] -= 1
+                # 턴 종료
+                self.next_turn()
+            else:
+                self.statusText.setText("설치 불가능한 지역입니다!")
+
+
+        else:  # 말이 움직이는 곳임
+            if self.board.player_clicked(y, x):
+                #말 이동
+                (i,j) = self.board.find_board(self.turn)
+                self.board.status[y][x] = self.turn
+                self.board.status[i][j] = 0
+                self.board_button[y][x].setStyleSheet("background-color : " + ("red " if self.turn == 2 else "blue"))
+                self.board_button[i][j].setStyleSheet("background-color : white")
+                self.next_turn()
+    
+    def find_btn(self, btn):
+        for i in range(17):
+            for j in range(17):
+                if self.board_button[i][j] == btn:
+                    return (i,j)
+
+    def next_turn(self):
+        (y, x) = self.board.find_board(self.turn)
+        if (3 - self.turn) * 16 == y:
+            self.statusText.setStyleSheet("color : " + ("red " if self.turn == 2 else "blue"))
+            self.statusText.setText(("Red " if self.turn == 2 else "Blue") + "의 승리!")
+            self.erase_4()
+            return
+        #erase 하기
+
+        self.turn = 5 - self.turn
+        self.erase_4()
+        self.board.whereCanMove(self.turn)
+        self.color_4()
+        
+        self.player1block.setText(str(self.wall_count[0]))
+        self.player2block.setText(str(self.wall_count[1]))
+        
+        self.statusText.setText("")
+        if self.turn == 2:
+            self.currentturn.setText("Red turn")
+            self.currentturn.setStyleSheet("color : Red")
+        else:
+            self.currentturn.setText("Blue turn")
+            self.currentturn.setStyleSheet("color : Blue")
+        pass
+
+    def erase_4(self):
+        for i in range(17):
+            for j in range(17):
+                if self.board.status[i][j] == 4:
+                    self.board.status[i][j] = 0
+                    self.board_button[i][j].setStyleSheet("background-color : white")
+
+    
+    def color_4(self):
+        for i in range(17):
+            for j in range(17):
+                if self.board.status[i][j] == 4:
+                    self.board_button[i][j].setStyleSheet("background-color : green")
+
+
+if __name__ == '__main__':
+    import sys
+    app = QApplication(sys.argv)
+    game = QuoridorGame()
+    game.show()
+    sys.exit(app.exec_())
